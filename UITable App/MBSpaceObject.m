693ce1127c4fd8c0dedf7d0cb0cf065968a37dfa
Michael Bloom <michaelbloom@macbookpro.local>
2014-06-01 21:52:03 -0400

    initWIthData method

--- UITable App/MBSpaceObject.m
+++ UITable App/MBSpaceObject.m
@@ -7,7 +7,38 @@
 //
 
 #import "MBSpaceObject.h"
+#import "AstronomicalData.h"
 
 @implementation MBSpaceObject
 
+-(id)init
+
+{
+    
+    self = [self initWithData:nil andImage:nil];
+    return self;
+    
+}
+
+-(id)initWithData:(NSDictionary*)data andImage:(UIImage*)image
+
+{
+    
+    self = [super init]; //helps setup objects
+    
+    // set the values for custom properties that you created in the .h
+    self.name = data[PLANET_NAME];
+    self.gravitationalForce = [data[PLANET_GRAVITY]floatValue];
+    self.diameter = [data[PLANET_DIAMETER]floatValue];
+    self.yearLength = [data[PLANET_YEAR_LENGTH]floatValue];
+    self.dayLength = [data[PLANET_DAY_LENGTH]floatValue];
+    self.temperature = [data[PLANET_TEMPERATURE]floatValue];
+    self.numberOfMoons = [data[PLANET_NUMBER_OF_MOONS]intValue];
+    self.nickname = data[PLANET_NICKNAME];
+    self.interestingFact = data[PLANET_INTERESTING_FACT];
+    
+    self.spaceImage = image;
+    
+    return self;
+}
 @end
