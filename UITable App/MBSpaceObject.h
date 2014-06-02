9faa0ff23c668957249e344e03010a6d70ab3c7f
Michael Bloom <michaelbloom@macbookpro.local>
2014-06-01 21:52:21 -0400

    variables

--- UITable App/MBSpaceObject.h
+++ UITable App/MBSpaceObject.h
@@ -10,4 +10,21 @@
 
 @interface MBSpaceObject : NSObject
 
+-(id)initWithData:(NSDictionary *)data andImage:(UIImage *)image;
+
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) NSString *name;
+@property (nonatomic) float gravitationalForce;
+@property (nonatomic) float diameter;
+@property (nonatomic) float yearLength;
+@property (nonatomic) float dayLength;
+@property (nonatomic) float temperature;
+@property (nonatomic) int numberOfMoons;
+@property (strong, nonatomic) NSString *nickname;
+@property (strong, nonatomic) NSString *interestingFact;
+
+@property (strong, nonatomic) UIImage *spaceImage;
+
+
 @end
