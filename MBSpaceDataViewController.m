8c2692a25f7b7c69de6137501b829c5543b43672
Michael Bloom <michaelbloom@Michaels-MacBook-Pro.local>
2014-06-09 23:58:11 -0400

    first commit

---
+++ MBSpaceDataViewController.m
@@ -0,0 +1,114 @@
+//
+//  MBSpaceDataViewController.m
+//  UITable App
+//
+//  Created by Michael Bloom on 6/4/14.
+//  Copyright (c) 2014 Michael Bloom. All rights reserved.
+//
+
+#import "MBSpaceDataViewController.h"
+
+@interface MBSpaceDataViewController ()
+
+@end
+
+@implementation MBSpaceDataViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor blackColor];
+    
+    self.tableView.backgroundColor = [UIColor clearColor];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableview Methods
+
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"DataCell"; //static means the variable will be kept entire application - start to finish
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath]; 
+    
+    
+    switch (indexPath.row)
+    {
+        case 0:
+            cell.textLabel.text = @"Nickname:";
+            cell.detailTextLabel.text = self.spaceObject.nickname;
+            break;
+            
+        case 1:
+            cell.textLabel.text = @"Diameter (km):";
+            cell.detailTextLabel.text = [NSString stringWithFormat:@"%f", self.spaceObject.diameter];
+            break;
+
+        case 2:
+            cell.textLabel.text = @"Gravitational Force: :";
+            cell.detailTextLabel.text = [NSString stringWithFormat:@"%f", self.spaceObject.gravitationalForce];
+            break;
+
+        case 3:
+            cell.textLabel.text = @"Length of Year in Days:";
+            cell.detailTextLabel.text = [NSString stringWithFormat:@"%f", self.spaceObject.yearLength];
+            break;
+
+
+        case 4:
+            cell.textLabel.text = @"Lenght of day Hours";
+            cell.detailTextLabel.text = [NSString stringWithFormat:@"%f", self.spaceObject.dayLength];
+            break;
+
+        case 5:
+            cell.textLabel.text = @"Temperature:";
+            cell.detailTextLabel.text = [NSString stringWithFormat:@"%f", self.spaceObject.temperature];
+            break;
+
+        case 6:
+            cell.textLabel.text = @"Number of Moons:";
+            cell.detailTextLabel.text = [NSString stringWithFormat:@"%i", self.spaceObject.numberOfMoons];
+            break;
+        case 7:
+            cell.textLabel.text = @"Interesting Fact:";
+            cell.detailTextLabel.text = self.spaceObject.interestingFact;
+            break;
+        default:break;
+            
+    }
+    
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 8; //return number of cells...
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
