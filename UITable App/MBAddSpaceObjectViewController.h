881fc9fc632096d69f6a32fcea4528ff0f313aaf
Michael Bloom <michaelbloom@Michaels-MacBook-Pro.local>
2014-06-09 23:58:32 -0400

    first commit

---
+++ UITable App/MBAddSpaceObjectViewController.h
@@ -0,0 +1,36 @@
+//
+//  MBAddSpaceObjectViewController.h
+//  UITable App
+//
+//  Created by Michael Bloom on 6/5/14.
+//  Copyright (c) 2014 Michael Bloom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MBSpaceObject.h"
+
+@protocol MBAddSpaceObjectViewControllerDelegate <NSObject>
+
+@required
+-(void)addSpaceObject;
+-(void)didCancel;
+
+@end
+
+@interface MBAddSpaceObjectViewController : UIViewController
+
+//weak - avoid retain cylces - both classes would be held and wouldn't be able to deallocate memory if was strong
+//id - object of type of unknown
+@property (weak,nonatomic) id <MBAddSpaceObjectViewControllerDelegate> delegate;
+
+@property (strong, nonatomic) IBOutlet UITextField *nameTextField;
+@property (strong, nonatomic) IBOutlet UITextField *nicknameTextField;
+@property (strong, nonatomic) IBOutlet UITextField *diameterTextField;
+@property (strong, nonatomic) IBOutlet UITextField *temperatureTextField;
+@property (strong, nonatomic) IBOutlet UITextField *numberOfMoonsTextField;
+@property (strong, nonatomic) IBOutlet UITextField *interestingFactTextField;
+
+- (IBAction)cancelButton:(UIButton *)sender;
+- (IBAction)addButton:(UIButton *)sender;
+
+@end
